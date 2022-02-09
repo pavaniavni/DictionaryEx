c7152689a6032326ccfe23eb276086c8e0f07838
pavani <pavaniavni456@gmail.com>
2022-02-09 10:40:35 +0530

    added DictinaryExample

---
+++ DictionaryEx/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections.Generics
+{
+     class DictionaryEx
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example of Dictionary");
+            Dictionary<int, string> IN = new Dictionary<int, string>();
+            Console.WriteLine("Enter name of languages : ");
+            for (int i = 1; i < 5; i++)
+            {
+                IN.Add(i, Console.ReadLine());
+            }
+            foreach (var item in IN)
+            {
+                Console.WriteLine(item.Key + "====>" + item.Value);
+            }
+
+        }
+    }
+}
